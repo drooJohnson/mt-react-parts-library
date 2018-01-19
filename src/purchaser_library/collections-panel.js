7ce846fb847336635cb70d6ab6a0095b0b512f74
Andrew Johnson <andrew@droojohnson.com>
2018-01-19 16:18:39 -0500

    Typo fix

--- src/purchaser_library/collections-panel.js
+++ src/purchaser_library/collections-panel.js
@@ -100,7 +100,7 @@ const SyncButton = styled(Button)`
 class A360Area extends React.Component {
   render(){
     return (
-      <SyncWrapper><SyncButton nature="info">Sync With A360</SyncButton></SyncWrapper>
+      <SyncWrapper><SyncButton nature="info" width="full">Sync With A360</SyncButton></SyncWrapper>
     )
   }
 }
