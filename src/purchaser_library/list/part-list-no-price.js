1b785c5cb3c676d8e3cf4d5daf703c11c98d6f02
Andrew Johnson <andrew@droojohnson.com>
2018-02-06 14:13:40 -0500

    Implement list view pricing unavailable popover

--- src/purchaser_library/list/part-list-no-price.js
+++ src/purchaser_library/list/part-list-no-price.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {keyframes} from 'styled-components'
 import styled from 'styled-components'
+import { Transition } from 'react-transition-group'
+import TetherComponent from 'react-tether'
+import zIndex from '../../components/utils/z-index'
+import { connect } from 'react-redux'
+
+let duration = 300;
 
 const Wrapper = styled.div`
 	flex-grow: 1;
@@ -50,19 +57,204 @@ const BlueLink = styled.a`
 	cursor: pointer;
 `
 
-const PartListNoPrice = ({ gridArea, hover, loading }) => {
-	return(
-		<Wrapper gridArea={gridArea} loading={loading} hover={hover}>
-			<GreyText>PREDICTED PRICE<br/>NOT AVAILABLE</GreyText>
-			<BlueLink>Why?</BlueLink>
-			<Chart hover={hover}/>
-		</Wrapper>
-	)}
+const PriceFeedback = styled.div`
+	margin-bottom: 12px;
+	display: block;
+	visibility: ${props => props.hover ? 'visible' : 'hidden' };
+	opacity: ${props => props.hover ? '1.0' : '0.0' };
+	transition: ${props => props.hover ? 'opacity 300ms ease' : 'opacity 300ms ease, visibility 0ms 300ms ease' };
+`
 
-PartListNoPrice.propTypes = {
-	gridArea: PropTypes.string,
-	hover: PropTypes.bool,
-	loading: PropTypes.bool,
+const IntercomIcon = styled.img`
+	margin-left: 4px;
+	color: #137DE5;
+	width: 11px;
+	height: 13px;
+	vertical-align: bottom;
+`
+
+let popoverBlockWrapperDefaultStyle = {
+	opacity: 0.0,
+	display: 'block',
 }
 
-export default PartListNoPrice
+let popoverBlockWrapperEntrance = keyframes`
+	0%{
+		display:flex;
+		transform:translateY(10px)  translateZ(0) scale(0.95);
+		opacity:0.0;
+	}
+	100%{
+		display:flex;
+		transform:translateY(0px)  translateZ(0);
+		opacity:1.0;
+	}
+`
+
+let popoverBlockWrapperExit = keyframes`
+	0%{
+		display:flex;
+		transform:translateY(0px)  translateZ(0);
+		opacity:1.0;
+	}
+	100%{
+		display:flex;
+		transform:translateY(10px)  translateZ(0) scale(0.95);
+		opacity:0.0;
+	}
+`
+
+let popoverBlockWrapperTransitionStyles = {
+	entering: { animation:`${popoverBlockWrapperEntrance} ${duration}ms ease 0s 1 normal both`, display:'flex' },
+	entered:  { display: 'flex', opacity:'1.0' },
+	exiting:  { animation:`${popoverBlockWrapperExit} ${duration}ms ease 0s 1 normal both`, display:'flex' },
+	exited:   { display:'none' },
+}
+
+const PopoverBlock = styled.div`
+	padding: 8px;
+	padding-left: 16px;
+	background-color: white;
+	z-index: ${zIndex['mid']};
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	transition: opacity ${duration}ms ease, transform ${duration}ms ease;
+	border-radius: 2px;
+	opacity: 1.0;
+	position: relative;
+`
+
+const PopoverBlockWrapper = styled.div`
+	display:flex;
+	flex-direction:column;
+	align-items:center;
+	justify-content:center;
+	font-family:"Proxima Nova";
+	max-width:240px;
+	filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.1));
+`
+
+const PopoverText = styled.p`
+	color: #666666;
+	font-size: 12px;
+	line-height: 16px;
+	margin-top:10px;
+	margin-bottom:10px;
+	margin-right:8px;
+`
+
+const CenterArrow = styled.div`
+	content: '';
+	display: block;
+	border: 8px solid transparent;
+	border-top: 8px solid white;
+	height: 1px;
+	width: 1px;
+	z-index: ${zIndex['mid']+10};
+`
+
+const PopoverTrigger = ({ open, handlePopoverClick, handlePopoverClose, partId, children }) => (
+	<TetherComponent
+		style={{zIndex: 200}}
+		attachment="bottom center"
+		targetAttachment="top center"
+		constraints={[
+			{
+				to: 'scrollParent',
+				pin: ['right','left']
+			},
+			{
+				to: 'scrollParent',
+				attachment: 'together'
+			}
+		]}>
+		{ /* First child: This is what the item will be tethered to */}
+		<BlueLink onClick={()=>{handlePopoverClick();}}>{children}</BlueLink>
+		{ /* Second child: If present, this item will be tethered to the the first child */ }
+		<Transition mountOnEnter={true} unmountOnExit={true} appear={true} in={open} timeout={{enter: 300, exit: duration}}>
+			{(state) => (
+				<PopoverBlockWrapper className='popoverBlockWrapper' style={{...popoverBlockWrapperDefaultStyle, ...popoverBlockWrapperTransitionStyles[state]}}>
+					<CenterArrow className="popoverArrowTop"/>
+					<PopoverBlock className={'listPopoverBlock'}>
+						<CloseSVG style={{display:'block',marginLeft:'auto',marginRight:'0',cursor:'pointer'}} onClick={handlePopoverClose}/>
+						<PopoverText>A predicted price may not be available for a part depending on a few factors. Make sure your part has a 3D file, material type/grade, and tolerance in part details. If a price is still not available, we may not be able to generate a price on your part details.</PopoverText>
+						<PriceFeedback hover={true}>
+							<BlueLink style={{fontSize:'12px',textDecoration:'none'}}>Feel free to chat with us</BlueLink> <IntercomIcon src="../assets/icons/intercom.svg"/>
+						</PriceFeedback>
+					</PopoverBlock>
+					<CenterArrow className="popoverArrowBottom"/>
+				</PopoverBlockWrapper>
+			)}
+		</Transition>
+	</TetherComponent>
+)
+
+const CloseSVG = ({style, onClick}) => (
+<svg width="11px" height="12px" viewBox="0 0 11 12" style={style} onClick={onClick}>
+    <g id="error" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g id="mt-part-index-LIST-ERROR-Copy" transform="translate(-1045.000000, -287.000000)" fill="#999999">
+            <g id="Group-3" transform="translate(830.000000, 278.000000)">
+                <g id="Group-6" transform="translate(215.000000, 9.000000)">
+                    <rect id="Rectangle-4" transform="translate(5.500000, 6.000000) rotate(45.000000) translate(-5.500000, -6.000000) " x="5" y="-1" width="1" height="14"></rect>
+                    <rect id="Rectangle-4-Copy" transform="translate(5.500000, 6.000000) rotate(-45.000000) translate(-5.500000, -6.000000) " x="5" y="-1" width="1" height="14"></rect>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
+)
+
+const ScrimClone = styled.span`
+  position:fixed;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+  z-index: ${zIndex['low']+1};
+`
+
+class PartListNoPrice extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			open: false,
+		}
+	}
+	render(){
+		let {open, hover, loading, handlePopoverClick, handlePopoverClose} = this.props;
+		return(
+			<React.Fragment>
+			<Wrapper loading={loading} hover={hover}>
+				<GreyText>PREDICTED PRICE<br/>NOT AVAILABLE</GreyText>
+				<PopoverTrigger open={open} handlePopoverClose={handlePopoverClose} handlePopoverClick={()=>{handlePopoverClick();this.props.scrimToggle(this);}}>Why?</PopoverTrigger>
+				<Chart hover={hover}/>
+			</Wrapper>
+			{ open && <ScrimClone id="scrimClone" onClick={()=>{handlePopoverClick();this.props.onScrimClick(this);}} {...this.state}/> }
+			</React.Fragment>
+	)
+}
+}
+PartListNoPrice.propTypes = {
+	hover: PropTypes.bool,
+	loading: PropTypes.bool,
+	open: PropTypes.bool,
+	handlePopoverClick: PropTypes.func,
+	handPopoverClose: PropTypes.func
+}
+
+export default connect(
+  null,
+  (dispatch) => ({
+    scrimToggle: (ref) => {
+      if (ref.state.open === false) {
+        ref.setState({open:true})
+      } else {
+        dispatch({type: 'HIDE_SCRIM'});
+        ref.setState({open:false});
+      }
+    },
+    onScrimClick: (ref) => {
+      dispatch({type: 'HIDE_SCRIM'});
+      ref.setState({open:false});
+    }
+  })
+)(PartListNoPrice);
